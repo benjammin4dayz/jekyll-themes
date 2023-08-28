f75a08a102f76705a530c012b02bae047dd0aaf1
benjammin4dayz <42326027+benjammin4dayz@users.noreply.github.com>
2023-08-28 18:41:53 -0400

    chore: use .txt instead of .gitkeep

--- flow.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-REM Create the build directory
-mkdir ..\jekyll-flow
-
-REM Copy all files from ./__dist to ./../build
-xcopy /E /I __dist ..\jekyll-flow
-
-echo Files copied successfully.
