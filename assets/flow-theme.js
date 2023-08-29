e7be67c8f34f5d38dd7117d06ebf6ed72a4d0c9d
benjammin4dayz <42326027+benjammin4dayz@users.noreply.github.com>
2023-08-29 11:52:49 -0400

    ref: StickyHeader modularity

--- assets/flow-theme.js
+++ assets/flow-theme.js
@@ -4,7 +4,7 @@
 class Site {
   static init() {
     // Site.info('site-info');
-    // Site.trueViewHeight(); //TODO: Look into excessive redraws on mobile, evaluate if this is needed
+    // Site.trueViewHeight(); //TODO: Evaluate if this is still needed
     Site.trackPageHash('.landing-nav a', 'active');
     Site.stickyHeader('landing-nav');
   }
@@ -193,39 +193,68 @@ class Site {
 
     class StickyHeader {
       constructor(elementId) {
-        this.header = elementId;
+        this.headerId = elementId;
         this.init();
       }
 
       init() {
-        this.origin = this._calculateInitialPosition();
-        this._onScroll(this._toggleSticky);
+        this.origin = this.calculateInitialPosition();
+        this._onScroll(this.toggleSticky);
       }
 
-      _calculateInitialPosition(el = this.header) {
+      calculateInitialPosition(el = this.header) {
         const rect = el.getBoundingClientRect();
         const top = rect.top + window.scrollY;
         const left = rect.left + window.scrollX;
         return { top, left };
       }
 
-      _toggleSticky(pos = window.scrollY) {
-        const [top, origin] = [this.offsetTop, this.origin.top];
-        pos > top ? this._addClass('sticky') : this._removeClass('sticky');
-        if (!this._isMobileDevice)
-          if (pos <= origin) this._removeClass('sticky');
+      toggleSticky(pos = window.scrollY) {
+        pos > this.offsetTop ? this._makeSticky() : this._makeNotSticky();
+        if (pos <= this.origin.top) this._makeNotSticky();
       }
+
+      _makeSticky() {
+        if (!this.header.classList.contains('sticky')) {
+          this._createPlaceholder();
+          this._addClass('sticky');
+        }
+      }
+
+      _makeNotSticky() {
+        if (this.header.classList.contains('sticky')) {
+          this._removePlaceholder();
+          this._removeClass('sticky');
+        }
+      }
+
+      _createPlaceholder() {
+        const placeholder = document.createElement('div');
+        placeholder.style.height = `${this.header.offsetHeight}px`;
+        this.header.parentNode.insertBefore(placeholder, this.header);
+        this.placeholder = placeholder;
+      }
+
+      _removePlaceholder() {
+        if (this.placeholder) {
+          this.placeholder.parentNode.removeChild(this.placeholder);
+          this.placeholder = null;
+        }
+      }
+
       _onScroll(callback) {
         window.addEventListener('scroll', () => callback.call(this));
       }
+
       _addClass(className) {
         this.header.classList.add(className);
       }
+
       _removeClass(className) {
         this.header.classList.remove(className);
       }
 
-      set header(id) {
+      set headerId(id) {
         if (id instanceof Element) return (this._header = id);
         const element = document.getElementById(id);
         element
@@ -240,17 +269,13 @@ class Site {
       get offsetTop() {
         return this.header.offsetTop;
       }
+
       set origin(pos) {
         this._origin = pos;
       }
+
       get origin() {
         return this._origin;
-      }
-
-      get _isMobileDevice() {
-        return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-          navigator.userAgent
-        );
       }
     }
 
